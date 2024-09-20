a658aacda7b52b101852746a476681ff81631fa2
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2024-09-20 11:30:49 -0500

    Added authentication for merchant app

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,17 +1,18 @@
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db/client"
+import { authOptions } from "../../lib/auth";
 
-const client = new PrismaClient();
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "abc",
-            name: "Abc"
-        }
-    })
-
+    const session = await getServerSession(authOptions);
+    if (session.user) {
+        return NextResponse.json({
+            user: session.user
+        })
+    }
     return NextResponse.json({
-        message: "Hi There"
+        message: "You are not logged in"
+    }, {
+        status: 403
     })
 }
