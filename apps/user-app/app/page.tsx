8936bf6b1dbc4c77fc8b689fc44dac691e7911b9
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2024-09-20 16:12:04 -0500

    Added UI

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-export default function Home() {
-  return (
-    <div></div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+
 }
